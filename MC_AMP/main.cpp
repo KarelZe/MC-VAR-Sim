09a0bd493e39f69b101c8f55eda5223d395b0154
Markus Bilz <markusbilz@t-online.de>
2018-04-24 18:03:39 +0100

    Fixed rng on large dataset

--- MC_AMP/main.cpp
+++ MC_AMP/main.cpp
@@ -29,7 +29,7 @@ marker_series markers;
 
 void report_accelerator(const accelerator a)
 {
-	const std::wstring bs[2] = {L"false", L"true"};
+	const std::wstring bs[2] = { L"false", L"true" };
 	std::wcout << ": " << a.description << " "
 		<< std::endl << "       device_path                       = " << a.device_path
 		<< std::endl << "       dedicated_memory                  = " << std::setprecision(4) << float(a.dedicated_memory) / (
@@ -83,6 +83,8 @@ Snippet is adapted from a Microsoft sample. See https://goo.gl/cU6b1X for detail
 */
 void box_muller_transform(float& u1, float& u2) restrict(amp)
 {
+	// limit minimum uniform random to > 0, as log(0) = n. d.
+	u1 = fast_math::fmax(u1, 0.00000000000001f);
 	const float r = fast_math::sqrt(-2.0f * fast_math::log(u1));
 	const float phi = 6.28318530717958f * u2;
 	u1 = r * fast_math::cos(phi);
@@ -92,7 +94,7 @@ void box_muller_transform(float& u1, float& u2) restrict(amp)
 /* This function calculates random paths using geometric brownian motion (GBM) for a given holding period. For
 details on geometric brownian motion see: https://goo.gl/lrCeLJ.
 */
-void generate_random_paths(const unsigned seed, const float initial_value, const float expected_return,
+void generate_random_paths(const float initial_value, const float expected_return,
                            const float volatility, const int trading_days, const int holding_period,
                            array<float>& endvalues)
 {
@@ -104,8 +106,8 @@ void generate_random_paths(const unsigned seed, const float initial_value, const
 	 * paths do not yield in more accurate simulation results. But results are already pretty stable at 65'536.
 	 */
 	const unsigned rank = endvalues.extent[0] > 65'536 ? 65'536 : endvalues.extent[0];
-	const extent<1> tinymt_extent(rank);
-	const tinymt_collection<1> rand_collection(tinymt_extent, seed);
+	const extent<1> tinymt_extent(1);
+	const tinymt_collection<1> rand_collection(tinymt_extent);
 
 	// flag for concurrency visualizer
 	markers.write_flag(normal_importance, L"generate random");
@@ -118,7 +120,8 @@ void generate_random_paths(const unsigned seed, const float initial_value, const
 			float s(0.0f);
 			float prev_s(initial_value);
 			// modify index if number is greater than size of tinymt_collection.
-			tinymt t = rand_collection[idx % 65'536];
+			tinymt t = rand_collection[index<1>(0)];
+			t.initialize(idx[0]);
 			// implementation follows a Geometric brownian motion model. See https://bit.ly/2HLeqPS for rationalle behind it.
 			// scale drift to timestep
 			const float daily_drift = expected_return / trading_days;
@@ -130,7 +133,7 @@ void generate_random_paths(const unsigned seed, const float initial_value, const
 			// generate path for entire holding period, write endprices back to vector
 			for (auto day(1); day <= holding_period / 2; day++)
 			{
-				// generate two random numbers and convert to normally distributed numbers
+				// generate two random numbers between 0-1 and convert to normally distributed numbers
 				auto z0 = t.next_single();
 				auto z1 = t.next_single();
 				box_muller_transform(z0, z1);
@@ -195,29 +198,29 @@ float min_element(array<float, 1>& src)
 		while (element_count >= TileSize)
 		{
 			parallel_for_each(compute_domain,
-			                  [=, &src, &dst](tiled_index<TileSize> tidx) restrict(amp)
-			                  {
-				                  // Use tile_static as a scratchpad memory.
-				                  tile_static float tile_data[TileSize];
+				[=, &src, &dst](tiled_index<TileSize> tidx) restrict(amp)
+			{
+				// Use tile_static as a scratchpad memory.
+				tile_static float tile_data[TileSize];
 
-				                  unsigned local_idx = tidx.local[0];
-				                  tile_data[local_idx] = padded_read(src, tidx.global);
-				                  tidx.barrier.wait();
+				unsigned local_idx = tidx.local[0];
+				tile_data[local_idx] = padded_read(src, tidx.global);
+				tidx.barrier.wait();
 
-				                  for (unsigned s = TileSize / 2; s > 0; s /= 2)
-				                  {
-					                  if (local_idx < s)
-					                  {
-						                  tile_data[local_idx] = fast_math::fmin(tile_data[local_idx], tile_data[local_idx + s]);
-					                  }
-					                  tidx.barrier.wait();
-				                  }
-				                  // Store the tile result in the global memory.
-				                  if (local_idx == 0)
-				                  {
-					                  dst[tidx.tile] = tile_data[0];
-				                  }
-			                  });
+				for (unsigned s = TileSize / 2; s > 0; s /= 2)
+				{
+					if (local_idx < s)
+					{
+						tile_data[local_idx] = fast_math::fmin(tile_data[local_idx], tile_data[local_idx + s]);
+					}
+					tidx.barrier.wait();
+				}
+				// Store the tile result in the global memory.
+				if (local_idx == 0)
+				{
+					dst[tidx.tile] = tile_data[0];
+				}
+			});
 			// Update the sequence length, swap source with destination.
 			element_count /= TileSize;
 			std::swap(src, dst);
@@ -244,8 +247,7 @@ the endvalue at rank 0.*/
 template <const int TileSize>
 void calculate_value_at_risk(std::vector<float>& host_end_values, const float initial_value,
                              const float expected_return,
-                             const float volatility, const int trading_days, const int holding_period,
-                             const int seed)
+                             const float volatility, const int trading_days, const int holding_period)
 {
 	// time taken to initialize, no copying of data, entire implementation uses array instead of array_view to be able to measure copying times as well (cmp. p. 131 AMP book) 
 	const auto start_initialize = the_amp_clock::now();
@@ -256,8 +258,7 @@ void calculate_value_at_risk(std::vector<float>& host_end_values, const float in
 	std::cout << std::setw(35) << std::left << "Initialize time: " << elapsed_time_initialize << std::endl;
 
 	// first kernel: generate random paths
-	generate_random_paths(seed, initial_value, expected_return, volatility, trading_days, holding_period,
-	                      gpu_end_values);
+	generate_random_paths(initial_value, expected_return, volatility, trading_days, holding_period, gpu_end_values);
 
 	gpu_end_values.accelerator_view.wait();
 	auto const end_kernel_one = the_amp_clock::now();
@@ -303,7 +304,7 @@ void warm_up()
 {
 	std::vector<float> paths(1024, 0);
 	// run kernel with minimal dataset
-	calculate_value_at_risk<4>(paths, 10.0f, 0.05f, 0.04f, 300, 10, 0);
+	calculate_value_at_risk<4>(paths, 10.0f, 0.05f, 0.04f, 300, 10);
 	std::cout <<
 		"------------------------------------- valid results starting from here -------------------------------------" << std::endl;
 } // warm_up
@@ -314,39 +315,39 @@ to this is suggested in the AMP book.
 */
 void run(const unsigned& tile_size, std::vector<float>& paths, const float initial_value = 10.0f,
          const float expected_return = 0.05f, const float volatility = 0.04f, const int trading_days = 300,
-         const int holding_period = 300, const int seed = 50'000)
+         const int holding_period = 300)
 {
 	switch (tile_size)
 	{
 	case 2:
-		calculate_value_at_risk<2>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<2>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 4:
-		calculate_value_at_risk<4>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<4>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 8:
-		calculate_value_at_risk<8>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<8>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 16:
-		calculate_value_at_risk<16>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<16>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 32:
-		calculate_value_at_risk<32>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<32>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 64:
-		calculate_value_at_risk<64>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<64>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 128:
-		calculate_value_at_risk<128>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<128>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 256:
-		calculate_value_at_risk<256>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<256>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 512:
-		calculate_value_at_risk<512>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<512>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	case 1024:
-		calculate_value_at_risk<1024>(paths, initial_value, expected_return, volatility, trading_days, holding_period, seed);
+		calculate_value_at_risk<1024>(paths, initial_value, expected_return, volatility, trading_days, holding_period);
 		break;
 	default:
 		assert(false);
@@ -415,35 +416,36 @@ int main(int argc, char* argv[])
 		file << "Initialize time,kernel one time,copying time,kernel two time" << std::endl;
 		//file << "step,";
 		*/
-	/* prepare body, dimensions is due to the limitations on tile size and tile count of c++ AMP.
-	See https://bit.ly/2qgCeTB for details.*/
-	/*
-	for (auto ps(1024); ps <= 524'288; ps *= 2) {
-		// initialize vector of problemsize, will contain endprices later.
-		std::vector<float>paths(ps);
+		/* prepare body, dimensions is due to the limitations on tile size and tile count of c++ AMP.
+		See https://bit.ly/2qgCeTB for details.*/
+		/*
+		for (auto ps(1024); ps <= 524'288; ps *= 2) {
+			// initialize vector of problemsize, will contain endprices later.
+			std::vector<float>paths(ps);
 
-		//std::vector<float>paths(ps);
-		// write problem size to first column
-		file << ps << ",";
-		//file << paths.size() << std::endl;
-		//for (auto ts(16); ts <= 1'024; ts *= 2) {
-			//run(ts, paths);
-			//for (auto i(0); i < 100; i++)
-		run(16, paths);
+			//std::vector<float>paths(ps);
+			// write problem size to first column
+			file << ps << ",";
+			//file << paths.size() << std::endl;
+			//for (auto ts(16); ts <= 1'024; ts *= 2) {
+				//run(ts, paths);
+				//for (auto i(0); i < 100; i++)
+			run(16, paths);
+		}
+		file << std::endl;
+		//}
+		// close file stream
+		file.close();
 	}
-	file << std::endl;
 	//}
-	// close file stream
-	file.close();
-}
-//}
-*/
+	*/
 	// test for concurrency visualizer
 	query_amp_support();
 	warm_up();
-
-	const auto ps(524'287), ts(128);
-	std::vector<float> paths(ps);
-	run(ts, paths);
+	for (auto i(1'024); i <= 524'287; i *= 2) {
+		const auto ps(i), ts(128);
+		std::vector<float> paths(ps);
+		run(ts, paths);
+	}
 	return 0;
 } // main
