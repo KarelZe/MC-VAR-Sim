47f843c61289dba0c7fd5ad2bd9a44fc0e95876f
Markus Bilz <markusbilz@t-online.de>
2018-04-24 10:36:22 +0100

    Reworked reduction algorithm to work with any problemsize

--- MC_AMP/main.cpp
+++ MC_AMP/main.cpp
@@ -5,12 +5,12 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
-#include <amp.h>
+#include <fstream>
 #include <string>
+#include <amp.h>
 #include <amp_tinymt_rng.h>
 #include <amp_math.h>
 #include <amp_algorithms.h>
-#include <fstream>
 #include <cvmarkersobj.h>
 #include <tclap/CmdLine.h>
 
@@ -29,7 +29,7 @@ marker_series markers;
 
 void report_accelerator(const accelerator a)
 {
-	const std::wstring bs[2] = { L"false", L"true" };
+	const std::wstring bs[2] = {L"false", L"true"};
 	std::wcout << ": " << a.description << " "
 		<< std::endl << "       device_path                       = " << a.device_path
 		<< std::endl << "       dedicated_memory                  = " << std::setprecision(4) << float(a.dedicated_memory) / (
@@ -49,9 +49,9 @@ void list_accelerators()
 	std::vector<accelerator> accls = accelerator::get_all();
 
 	// iterates over all accelerators and print characteristics
-	for (const auto& a : accls)
+	for (const auto& accl : accls)
 	{
-		report_accelerator(a);
+		report_accelerator(accl);
 	}
 
 	const accelerator acc = accelerator(accelerator::default_accelerator);
@@ -78,13 +78,13 @@ void query_amp_support()
 /*
 This function transforms uniformly distributed variables to normally distributed variables
 using the Cartesian form Box Muller transform. Box Muller is inferior in speed to Ziggurat
-algorithm but simpler to implemnt. That's why I've chosen Box Muller over Ziggurat algorithm.
+algorithm but simpler to implement. That's why I've chosen Box Muller over Ziggurat algorithm.
 Snippet is adapted from a Microsoft sample. See https://goo.gl/cU6b1X for details.
 */
 void box_muller_transform(float& u1, float& u2) restrict(amp)
 {
 	const float r = fast_math::sqrt(-2.0f * fast_math::log(u1));
-	const float phi = 2.0f * 3.14159265358979f * u2;
+	const float phi = 6.28318530717958f * u2;
 	u1 = r * fast_math::cos(phi);
 	u2 = r * fast_math::sin(phi);
 } // box_muller_transform
@@ -93,19 +93,19 @@ void box_muller_transform(float& u1, float& u2) restrict(amp)
 details on geometric brownian motion see: https://goo.gl/lrCeLJ.
 */
 void generate_random_paths(const unsigned seed, const float initial_value, const float expected_return,
-	const float volatility, const int trading_days, const int holding_period,
-	array<float>& endvalues)
+                           const float volatility, const int trading_days, const int holding_period,
+                           array<float>& endvalues)
 {
 	// validate that given input is optimal
-	assert(holdingPeriod % 2 == 0);
+	assert(holding_period % 2 == 0 && holding_period >= 2);
 
 	/* tinymt_collection is a wrapper around concurrency::array. Its maximum extent is at 65'536 due to the way how the rng
-	 * is implemented. It copies precalculated numbers into an concurrency::array. Total number of predefined numbers is 65'536.
-	 * This impacts the calculation in a way, that more paths do not yield in more accurate simulation results.
+	 * is implemented. The size of the tinymt_collection is at max 65'536. This impacts the calculation in a way, that more
+	 * paths do not yield in more accurate simulation results. But results are already pretty stable at 65'536.
 	 */
-	const unsigned rank = holding_period * endvalues.extent[0] > 65'536 ? 65'536 : holding_period * endvalues.extent[0];
-	const extent<1> tiny_e(rank);
-	const tinymt_collection<1> rand_collection(tiny_e, seed);
+	const unsigned rank = endvalues.extent[0] > 65'536 ? 65'536 : endvalues.extent[0];
+	const extent<1> tinymt_extent(rank);
+	const tinymt_collection<1> rand_collection(tinymt_extent, seed);
 
 	// flag for concurrency visualizer
 	markers.write_flag(normal_importance, L"generate random");
@@ -117,8 +117,8 @@ void generate_random_paths(const unsigned seed, const float initial_value, const
 		{
 			float s(0.0f);
 			float prev_s(initial_value);
-			// modify index if number is greater than size of tinymt_collection
-			auto t = rand_collection[idx % 65'535];
+			// modify index if number is greater than size of tinymt_collection.
+			tinymt t = rand_collection[idx % 65'536];
 			// implementation follows a Geometric brownian motion model. See https://bit.ly/2HLeqPS for rationalle behind it.
 			// scale drift to timestep
 			const float daily_drift = expected_return / trading_days;
@@ -154,26 +154,37 @@ void generate_random_paths(const unsigned seed, const float initial_value, const
 } // generate_random_paths
 
 
+/*
+* Function tries reading from array_view. If index is part of array_view it's value is returned, otherwise the greatest float.
+* Implementation inspired by the AMP book.
+*/
+float padded_read(const array<float, 1>& src, const index<1>& idx) restrict(cpu, amp)
+{
+	return src.extent.contains(idx) ? src[idx] : FLT_MAX;
+} // padded_read
+
 /* This function returns the smallest element of an array_view calculated using an
-map reduce approach. The major part is calculated on the gpu. While the summing
-of the tiles is done on the gpu. In case of an error 0 is returned.
+map reduce approach. The major part is calculated on the gpu. In case of an error 0 is returned.
 Tile size needs to be known at compile time. That's why I am using template arguments
 here.
 */
 template <const int TileSize>
-float min_element(array<float, 1>& src, int element_count)
+float min_element(array<float, 1>& src)
 {
-	// check for max tile size
-	assert(tile_size >= 2 && tile_size <= 1'024);
-	// tile_size and tile_count are not matching element_count
-	assert(elementCount % TS == 0);
-	// element_count is not valid.
-	assert(elementCount < 0 && elementCount <= INT_MAX);
-	// check if number of tiles is <= 65k, which is the max in AMP
-	assert(elementCount / TS < 65'536);
+	// pad compute_domain and expand to full tiles if necessary
+	tiled_extent<TileSize> compute_domain = src.extent.tile<TileSize>();
+	compute_domain = compute_domain.pad();
+	int element_count = compute_domain.size();
 
 	// Using arrays as temporary memory. Array holds at least one element
 	array<float, 1> dst(element_count / TileSize ? element_count / TileSize : 1);
+
+	// check for max tile size
+	assert(TileSize >= 2 && TileSize <= 1'024);
+	// element_count is not valid.
+	assert(element_count < 0 && element_count <= INT_MAX);
+	// check if number of tiles is <= 65k, which is the max in AMP
+	assert(element_count / TileSize < 65'536);
 
 	markers.write_flag(normal_importance, L"reduce");
 
@@ -181,34 +192,32 @@ float min_element(array<float, 1>& src, int element_count)
 	{
 		// Reduce using parallel_for_each as long as the sequence length
 		// is evenly divisable to the number of threads in the tile.
-		// todo: rethink while loop condition to support padded compute domains
-		while (element_count % TileSize == 0)
+		while (element_count >= TileSize)
 		{
-			// todo: pad compute domain
-			parallel_for_each(extent<1>(element_count).tile<TileSize>(),
-				[=, &src, &dst](tiled_index<TileSize> tidx) restrict(amp)
-			{
-				// Use tile_static as a scratchpad memory.
-				tile_static float tile_data[TileSize];
+			parallel_for_each(compute_domain,
+			                  [=, &src, &dst](tiled_index<TileSize> tidx) restrict(amp)
+			                  {
+				                  // Use tile_static as a scratchpad memory.
+				                  tile_static float tile_data[TileSize];
 
-				unsigned local_idx = tidx.local[0];
-				tile_data[local_idx] = src[tidx.global];
-				tidx.barrier.wait();
+				                  unsigned local_idx = tidx.local[0];
+				                  tile_data[local_idx] = padded_read(src, tidx.global);
+				                  tidx.barrier.wait();
 
-				for (unsigned s = TileSize / 2; s > 0; s /= 2)
-				{
-					if (local_idx < s)
-					{
-						tile_data[local_idx] = fast_math::fmin(tile_data[local_idx], tile_data[local_idx + s]);
-					}
-					tidx.barrier.wait();
-				}
-				// Store the tile result in the global memory.
-				if (local_idx == 0)
-				{
-					dst[tidx.tile] = tile_data[0];
-				}
-			});
+				                  for (unsigned s = TileSize / 2; s > 0; s /= 2)
+				                  {
+					                  if (local_idx < s)
+					                  {
+						                  tile_data[local_idx] = fast_math::fmin(tile_data[local_idx], tile_data[local_idx + s]);
+					                  }
+					                  tidx.barrier.wait();
+				                  }
+				                  // Store the tile result in the global memory.
+				                  if (local_idx == 0)
+				                  {
+					                  dst[tidx.tile] = tile_data[0];
+				                  }
+			                  });
 			// Update the sequence length, swap source with destination.
 			element_count /= TileSize;
 			std::swap(src, dst);
@@ -231,12 +240,12 @@ float min_element(array<float, 1>& src, int element_count)
 } // min_element
 
 /* This function calculates the value at risk at a confidence level of 100 % by calling the generate_random_paths function and by extracting
-the endvalue at rank 0. The functionality is similar to stl::min_element()*/
+the endvalue at rank 0.*/
 template <const int TileSize>
 void calculate_value_at_risk(std::vector<float>& host_end_values, const float initial_value,
-	const float expected_return,
-	const float volatility, const int trading_days, const int holding_period,
-	const int seed)
+                             const float expected_return,
+                             const float volatility, const int trading_days, const int holding_period,
+                             const int seed)
 {
 	// time taken to initialize, no copying of data, entire implementation uses array instead of array_view to be able to measure copying times as well (cmp. p. 131 AMP book) 
 	const auto start_initialize = the_amp_clock::now();
@@ -248,7 +257,8 @@ void calculate_value_at_risk(std::vector<float>& host_end_values, const float in
 
 	// first kernel: generate random paths
 	generate_random_paths(seed, initial_value, expected_return, volatility, trading_days, holding_period,
-		gpu_end_values);
+	                      gpu_end_values);
+
 	gpu_end_values.accelerator_view.wait();
 	auto const end_kernel_one = the_amp_clock::now();
 	const auto elapsed_time_kernel_one = duration_cast<milliseconds>(end_kernel_one - end_initialize).count();
@@ -262,7 +272,7 @@ void calculate_value_at_risk(std::vector<float>& host_end_values, const float in
 	std::cout << std::setw(35) << std::left << "copying time: " << elapsed_time_copying << std::endl;
 
 	// second kernel: rearrange elements to obtain element at rank 0
-	auto min_result = min_element<TileSize>(gpu_end_values, host_end_values.size());
+	auto min_result = min_element<TileSize>(gpu_end_values);
 	gpu_end_values.accelerator_view.wait();
 	const auto end_kernel_two = the_amp_clock::now();
 	const auto elapsed_time_kernel_two = duration_cast<milliseconds>(end_kernel_two - end_copy).count();
@@ -273,7 +283,7 @@ void calculate_value_at_risk(std::vector<float>& host_end_values, const float in
 
 	// write time to file
 	//file << elapsed_time_kernel_two << ",";
-	file << elapsed_time_initialize << "," << elapsed_time_kernel_one << "," << elapsed_time_copying << "," << elapsed_time_kernel_two << std::endl;
+	//file << elapsed_time_initialize << "," << elapsed_time_kernel_one << "," << elapsed_time_copying << "," << elapsed_time_kernel_two << std::endl;
 	//file << "Initialize" << "," << elapsed_time_initialize << std::endl;
 	//file << "Kernel one" << "," << elapsed_time_kernel_one << std::endl;
 	//file << "Copying" << "," << elapsed_time_copying << std::endl;
@@ -293,10 +303,9 @@ void warm_up()
 {
 	std::vector<float> paths(1024, 0);
 	// run kernel with minimal dataset
-	calculate_value_at_risk<4>(paths, 10.0f, 0.05f, 0.04f, 300, 300, 7859);
+	calculate_value_at_risk<4>(paths, 10.0f, 0.05f, 0.04f, 300, 10, 0);
 	std::cout <<
-		"------------------------------------- valid results starting from here -------------------------------------" << std
-		::endl;
+		"------------------------------------- valid results starting from here -------------------------------------" << std::endl;
 } // warm_up
 
 /* This is wrapper function around calculate_value_at_risk. It lets the tile_size dynamically
@@ -304,8 +313,8 @@ by using template parameters. The tilesize must be known at compile time. An app
 to this is suggested in the AMP book.
 */
 void run(const unsigned& tile_size, std::vector<float>& paths, const float initial_value = 10.0f,
-	const float expected_return = 0.05f, const float volatility = 0.04f, const int trading_days = 300,
-	const int holding_period = 300, const int seed = 50'000)
+         const float expected_return = 0.05f, const float volatility = 0.04f, const int trading_days = 300,
+         const int holding_period = 300, const int seed = 50'000)
 {
 	switch (tile_size)
 	{
@@ -358,7 +367,7 @@ int main(int argc, char* argv[])
 												 0.05f, "float");
 		TCLAP::ValueArg<int> trading_days("t", "trading_days", "Annual trading days", false, 300, "int");
 		TCLAP::ValueArg<int> holding_period("d", "duration", "Duration of the investment", false, 300, "int");
-		TCLAP::ValueArg<int> seed("s", "seed", "Seed for random number generator", false, 7'859, "int");
+		TCLAP::ValueArg<int> seed("s", "seed", "Seed for random number generator", false, 50'000, "int");
 		TCLAP::ValueArg<int> paths("p", "paths", "Number of paths", false, 1'024, "int");
 		TCLAP::ValueArg<int> tile_size("x", "tile_size", "Size of tiles", false, 16, "int");
 		cmd.add(initial_value);
@@ -388,11 +397,11 @@ int main(int argc, char* argv[])
 		std::cout << "error: " << e.error() << " for arg " << e.argId() << std::endl;
 	}
 	*/
+	/*
 	// Check AMP support
 	query_amp_support();
 	// run kernel once on small dataset to supress effects of lazy init and jit.
 	warm_up();
-
 	// start multi comparsion
 	//for (auto ps(1024); ps <= 524'288; ps *= 2) {
 		//file.open("measures_boxplot_"+ std::to_string(ps) + ".csv", std::ios::out);
@@ -405,37 +414,36 @@ int main(int argc, char* argv[])
 		// file << std::endl;
 		file << "Initialize time,kernel one time,copying time,kernel two time" << std::endl;
 		//file << "step,";
-
-		/* prepare body, dimensions is due to the limitations on tile size and tile count of c++ AMP.
-		See https://bit.ly/2qgCeTB for details.*/
-
-		for (auto ps(1024); ps <= 524'288; ps *= 2) {
-			// initialize vector of problemsize, will contain endprices later.
-			std::vector<float>paths(ps);
-
-			//std::vector<float>paths(ps);
-			// write problem size to first column
-			file << ps << ",";
-			//file << paths.size() << std::endl;
-			//for (auto ts(16); ts <= 1'024; ts *= 2) {
-				//run(ts, paths);
-				//for (auto i(0); i < 100; i++)
-			run(16, paths);
-		}
-		file << std::endl;
-		//}
-		// close file stream
-		file.close();
-	}
-	//}
-
-	// test for concurrency visualizer
+		*/
+	/* prepare body, dimensions is due to the limitations on tile size and tile count of c++ AMP.
+	See https://bit.ly/2qgCeTB for details.*/
 	/*
+	for (auto ps(1024); ps <= 524'288; ps *= 2) {
+		// initialize vector of problemsize, will contain endprices later.
+		std::vector<float>paths(ps);
+
+		//std::vector<float>paths(ps);
+		// write problem size to first column
+		file << ps << ",";
+		//file << paths.size() << std::endl;
+		//for (auto ts(16); ts <= 1'024; ts *= 2) {
+			//run(ts, paths);
+			//for (auto i(0); i < 100; i++)
+		run(16, paths);
+	}
+	file << std::endl;
+	//}
+	// close file stream
+	file.close();
+}
+//}
+*/
+	// test for concurrency visualizer
 	query_amp_support();
 	warm_up();
-	int ps(524'288), ts(128);
-	std::vector<float>paths(ps);
+
+	const auto ps(524'287), ts(128);
+	std::vector<float> paths(ps);
 	run(ts, paths);
-	*/
 	return 0;
 } // main
